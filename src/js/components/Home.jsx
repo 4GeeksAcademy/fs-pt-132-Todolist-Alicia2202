df80ff21eba63f230facb21413ca4b1dfb5e46c4
Alicia2202 <alicialf02@gmail.com>
2026-03-20 20:29:18 +0000

    Chore: Borrado código comentado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,15 +1,13 @@
 import React, {useEffect, useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Form from "./Form";
 import List from "./List";
 
 //create your first component
 const Home = () => {
 	const [dataList, setDataList] = useState([])
-	// const [showList, setShowList] = useState(false);
-
+	
 	const upDateList = (newInputValue) => {
 		if (newInputValue.trim() === "") return;
 		setDataList([...dataList, newInputValue]);
@@ -19,13 +17,6 @@ const Home = () => {
 		const newTask = dataList.filter((_, i) => i !== index);
 		setDataList(newTask);
 	};
-
-	// useEffect(()=>{
-	// 	if(dataList.length>0) return setShowList(true);
-	// 	return setShowList(false)
-	// 	}, [dataList])
-
-	// const showList = dataList.length > 0
 
 	return (
 		<div className="container d-flex flex-column  align-items-center">
